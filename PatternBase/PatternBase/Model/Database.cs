9c75f360919157ce84f0e962ca1a6048de35e776
Jelle Luteijn <jelle.luteijn@gmail.com>
2014-12-13 20:37:57 +0100

    model classen

---
+++ PatternBase/PatternBase/Model/Database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternBase.Model
+{
+    class Database
+    {
+        public List<Scope> scopes = new List<Scope>();
+        public List<Purpose> purposes = new List<Purpose>();
+        public List<Pattern> patterns = new List<Pattern>();
+    }
+}
---
+++ PatternBase/PatternBase/Model/Pattern.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternBase.Model
+{
+    class Pattern
+    {
+        public string name;
+        public string context;
+        public string problem;
+        public string solution;
+        public string description;
+        public string consequence;
+        public Purpose hasPurpose;
+        public List<Scope> hasScope = new List<Scope>();
+    }
+}
