a2356a46ea22dd828d2c3579ca25c365a77b8274
Jelle Luteijn <jelle.luteijn@gmail.com>
2014-12-13 22:21:34 +0100

    grote aanpassen

--- PatternBase/PatternBase/FrmEditor.Designer.cs
+++ PatternBase/PatternBase/FrmEditor.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 761);
+            this.Name = "FrmEditor";
             this.Text = "frmEditor";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
