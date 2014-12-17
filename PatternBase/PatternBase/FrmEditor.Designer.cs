c962906c3f50d44492edc5940a576da6fb70f129
Jelle Luteijn <jelle.luteijn@gmail.com>
2014-12-17 15:38:19 +0100

    credits

--- PatternBase/PatternBase/FrmEditor.Designer.cs
+++ PatternBase/PatternBase/FrmEditor.Designer.cs
@@ -47,6 +47,11 @@
             this.label3 = new System.Windows.Forms.Label();
             this.lbEditPurpose = new System.Windows.Forms.ListBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblDevider
@@ -224,11 +229,61 @@
             this.label4.TabIndex = 33;
             this.label4.Text = "Purpose";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(499, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 39;
+            this.label5.Text = "Created by";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(499, 261);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 13);
+            this.label6.TabIndex = 40;
+            this.label6.Text = "Simon Whiteley";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(499, 278);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 13);
+            this.label7.TabIndex = 41;
+            this.label7.Text = "Jelle Luteijn";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(499, 295);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 13);
+            this.label8.TabIndex = 42;
+            this.label8.Text = "IV2C";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(502, 312);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(207, 13);
+            this.label11.TabIndex = 43;
+            this.label11.Text = "HU: University of applied sciences Utrecht";
+            // 
             // FrmEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 576);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.lbEditPattern);
             this.Controls.Add(this.Pattern);
             this.Controls.Add(this.lbEditScope);
@@ -277,5 +332,10 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ListBox lbEditPurpose;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label11;
     }
 }
